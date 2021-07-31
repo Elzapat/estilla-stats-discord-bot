a1600bc2bdf17e653d4f0ce0575f22c95569e495
Morgan Van Amerongen <mistigri.van@gmail.com>
2021-07-31 23:15:52 +0200

    uh i commited the wrong file last time. now it's the real removed padding zeros on formatted time from minecraft ticks

--- src/utils.rs
+++ src/utils.rs
@@ -136,8 +136,8 @@ pub fn minecraft_ticks_to_formatted_time(ticks: u64) -> String {
 
     let days_s = if days > 0 { format!("{}d ", days) } else { "".to_string() }; 
     let hours_s = if days > 0 || hours > 0 { format!("{}h ", hours) } else { "".to_string() };
-    let minutes_s = if days > 0 || hours > 0 || minutes > 0 { format!("{:02}m ", minutes) } else { "".to_string() };
-    let seconds_s = format!("{:02}s", seconds);
+    let minutes_s = if days > 0 || hours > 0 || minutes > 0 { format!("{}m ", minutes) } else { "".to_string() };
+    let seconds_s = format!("{}s", seconds);
 
     format!("{}{}{}{}", days_s, hours_s, minutes_s, seconds_s)
 }
